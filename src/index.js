cc6e4ca3aed9b946c97baebba8cc74db61a35320
jonathancsr <jonathancsrr@gmail.com>
2020-02-27 23:04:24 -0300

    Todo add button working

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-const listOfToDos = document.querySelector('ul');
-const inputValue = document.querySelector('input');
-const addButton = document.querySelector('button')
-var toDos = [
-
-]
+const listOfToDos = document.querySelector('#app ul');
+const inputValue = document.querySelector('#app input');
+const addButton = document.querySelector('#app button')
+var toDos = [];
 
 function renderToDos(){
+	listOfToDos.innerHTML = '';
+
 	for(todo of toDos){
 		var toDo = document.createElement('li');
 		var edit = document.createElement('a');
 		var deleteButton = document.createElement('a');
-
-		toDo.textContent = todo;
-	
+		var todoText = document.createTextNode(todo);
+		
+		toDo.appendChild(todoText);
 		toDo.appendChild(edit);
 		toDo.appendChild(deleteButton);
 		listOfToDos.appendChild(toDo);	
 	}
 }
+
 function addToList() {
 	toDos.push(inputValue.value);
+	inputValue.value = "";
 	renderToDos();
 }
+
+addButton.onclick = addToList;
